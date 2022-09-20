695937ed2cd463fbe4a0e6adbb57c88334f77b56
UserGitHub37 <bum37@mail.ru>
2022-09-20 15:04:46 +0300

    fix: fix a Techs for screen width 320

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -8,8 +8,8 @@ function Techs () {
       <ContainerWrapper className={"container-wrapper__color_black"}>
         <div className="techs__container">
           <h2 className="techs__title">Технологии</h2>
-          <h3 className="techs__subtitle">7 технологий</h3>
           <div className="techs__line"></div>
+          <h3 className="techs__subtitle">7 технологий</h3>
           <p className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
           <ul className="techs__cards-wrapper">
             <li className="techs__card"><p className="techs__card-item">HTML</p></li>
