3ce3d6145003e2f5c7b4948b5e109fee9cfae533
UserGitHub37 <bum37@mail.ru>
2022-09-21 19:19:21 +0300

    feat: create a AboutMe for screen width 320

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe () {
     <section className="about-me">
       <ContainerWrapper className={'container-wrapper__color_black'}>
         <div className="about-me__container">
-          <h2 className="about-me__title">Студент</h2>
+          <h2 className="about-me__heading">Студент</h2>
           <div className="about-me__line"></div>
           <div className="about-me__card">
             <img src={avaPath} alt="Фото студента" className="about-me__ava" />
@@ -17,7 +17,7 @@ function AboutMe () {
               <h2 className="about-me__title">Андрей</h2>
               <h3 className="about-me__subtitle">Фронтенд-разработчик</h3>
               <p className="about-me__paragraph">
-                Я родился и живу в Москве, закончил факультет "Автомобили и тракторы" МГТУ "МАМИ" (Московский политех). До моего увлечения фронтенд разработкой 4 года проработал в техподдержке и 5 лет системным администратором. Выбрал фронтенд, потому что нравится процесс создания веб-сайтов, это работа творческая, ты сразу видишь результат своей работы и в отличие от работы системным администратором можешь продемонстрировать этот результат.
+                Я родился и живу в Москве, закончил факультет "Автомобили и тракторы" МГТУ "МАМИ" (Московский политех). До моего увлечения фронтенд разработкой 4 года проработал в техподдержке и 5 лет системным администратором. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos at id ad nemo nostrum excepturi recusandae rem iusto, consequuntur facere possimus odit libero numquam eaque in enim repudiandae minus mollitia?
               </p>
               <a href="https://github.com/UserGitHub37" className="about-me__link">Github</a>
             </div>
