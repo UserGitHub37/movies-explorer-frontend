bcd01abe02f4a0f13458d871b25231f7bd3e9460
UserGitHub37 <bum37@mail.ru>
2022-09-22 22:13:14 +0300

    feat: create a Footer for screen width 320

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,7 @@ import AboutProject from '../Main/AboutProject/AboutProject';
 import Techs from '../Main/Techs/Techs';
 import AboutMe from '../Main/AboutMe/AboutMe';
 import Portfolio from '../Main/Portfolio/Portfolio';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
             <Techs/>
             <AboutMe/>
             <Portfolio/>
+            <Footer/>
           </>
         }
       />
