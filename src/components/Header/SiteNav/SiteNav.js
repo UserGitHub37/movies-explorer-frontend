f0f7ec06bd5f8f3b3a20fb4b7e0af64493be510d
UserGitHub37 <bum37@mail.ru>
2022-09-27 02:51:08 +0300

    fix: fix the burger menu

--- src/components/Header/SiteNav/SiteNav.js
+++ src/components/Header/SiteNav/SiteNav.js
@@ -9,15 +9,40 @@ function SiteNav () {
 
   return (
     <>
-      <nav className='site-nav'>
-        <ul className='site-nav__links'>
-          <li className='site-nav__item-link'><Link to='/' className='site-nav__link'>Главная</Link></li>
-          <li className='site-nav__item-link'><Link to='/movies' className='site-nav__link site-nav__link_active'>Фильмы</Link></li>
-          <li className='site-nav__item-link'><Link to='/saved-movies' className='site-nav__link'>Сохраненные фильмы</Link></li>
+      <nav className="site-nav">
+        <ul className="site-nav__links">
+          <li className="site-nav__item-link">
+            <Link to="/" className="site-nav__link">
+              Главная
+            </Link>
+          </li>
+          <li className="site-nav__item-link">
+            <Link to="/movies" className="site-nav__link site-nav__link_active">
+              Фильмы
+            </Link>
+          </li>
+          <li className="site-nav__item-link">
+            <Link to="/saved-movies" className="site-nav__link">
+              Сохраненные фильмы
+            </Link>
+          </li>
         </ul>
       </nav>
-      <button className="account-btn"><img src={accBtnlogoPath} alt='Логотип кнопки' className='account-btn__logo'/>Аккаунт</button>
-      <button className="burger-btn"><img src={burgerImgPath} alt='Логотип бургерного меню' className='burger-btn__logo'/></button>
+      <button className="account-btn">
+        <img
+          src={accBtnlogoPath}
+          alt="Логотип кнопки"
+          className="account-btn__logo"
+        />
+        Аккаунт
+      </button>
+      <button className="burger-btn">
+        <img
+          src={burgerImgPath}
+          alt="Логотип бургерного меню"
+          className="burger-btn__logo"
+        />
+      </button>
     </>
   );
 }
