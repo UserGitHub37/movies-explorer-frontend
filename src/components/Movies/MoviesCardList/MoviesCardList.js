92dc9213830f01fcf495ad72051d47af9ee6feb0
UserGitHub37 <bum37@mail.ru>
2022-09-29 00:30:16 +0300

    feat: create a MoviesCardList for screen width 768

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,12 +1,37 @@
+import { useState, useEffect } from 'react';
 import { cards } from '../../../utils/movies'
+import useViewport from '../../../hooks/useViewport';
 import './MoviesCardList.css';
 import Card from '../MoviesCard/MoviesCard';
 
 function MoviesCardList () {
+  const { width } = useViewport();
+
+  const [numberOfCards, setNumberOfCards] = useState(5);
+
+  useEffect(() => {
+    const breakpoint = {
+      sm: 630,
+      md: 930,
+      lg: 1280,
+    };
+
+    if (width < breakpoint.sm) {
+      setNumberOfCards(5);
+    } else if (width < breakpoint.md) {
+      setNumberOfCards(8);
+    } else if (width < breakpoint.lg) {
+      setNumberOfCards(12);
+    } else {
+      setNumberOfCards(16);
+    }
+  }, [width])
+
+
   return (
     <section className="card-list" aria-label="Фильмы">
       <ul className="card-list__wrapper">
-        {cards.slice(0, 5).map((card) => (
+        {cards.slice(0, numberOfCards).map((card) => (
           <Card
             key={card.id}
             card={card}
