d9616913b2ae6cec7a22d1ea65ce980f2dd76f0b
UserGitHub37 <bum37@mail.ru>
2022-10-02 06:21:51 +0300

    feat: create register page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import Footer from '../Footer/Footer';
 import SearchForm from '../common/SearchForm/SearchForm';
 import Preloader from '../common/Preloader/Preloader';
 import MoviesCardList from '../common/MoviesCardList/MoviesCardList';
+import Register from '../Register/Register';
 
 function App() {
   return (
@@ -71,6 +72,14 @@ function App() {
           </>
         }
       />
+
+      <Route
+        path="/signup"
+        element={
+          <Register />
+        }
+      />
+
     </Routes>
   );
 }
