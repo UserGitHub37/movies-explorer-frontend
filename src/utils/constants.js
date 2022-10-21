d943a67f811fe65c8a35a45c44498428477bb47c
UserGitHub37 <bum37@mail.ru>
2022-10-21 05:06:17 +0300

    chore: correct main API URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 //Мой API с регистрацией, авторизацией и сохраненными фильмами пользователей
-export const MAIN_API_BASE_URL = 'http://localhost:3000/';
-// export const MAIN_API_BASE_URL = 'api.movies.thirtyseven.nomoredomains.sbs';
+//export const MAIN_API_BASE_URL = 'http://localhost:3000/';
+export const MAIN_API_BASE_URL = 'api.movies.thirtyseven.nomoredomains.sbs';
 
 
 //Сторонний API с базой фильмов
